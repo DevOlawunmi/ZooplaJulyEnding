27d67acfeec195c90ca42be3caa62944f34c80ca
Ola <olawunmi263@yahoo.com>
2020-07-28 06:02:29 +0100

    Browser class added

---
+++ src/test/java/co/uk/zoopla/common/Browsers.java
@@ -0,0 +1,52 @@
+package co.uk.zoopla.common;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Browsers extends DriverLibrary {
+    private WebDriver initChrome(){
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+    private WebDriver initFireFox(){
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
+            }
+//     private WebDriver initSafari(){
+//
+//     }
+
+     private WebDriver initHeadless() {
+        WebDriverManager.chromedriver().setup();
+         ChromeOptions options = new ChromeOptions();
+         options.addArguments("--disable-gpu","headless");
+         return new ChromeDriver();
+     }
+
+     public void launchBrowser(String browser){
+        switch (browser){
+            case "Chrome": driver = initChrome();
+            break;
+            case "Firefox": driver = initFireFox();
+            break;
+            case "Headless": driver = initHeadless();
+            break;
+            default:driver = initChrome();
+            break;
+
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+     }
+     public void closeBrowser(){
+        driver.manage().deleteAllCookies();
+        driver.quit();
+     }
+}
